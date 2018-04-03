c1d0ebc6e19d8e0315b615607dc12217bac2f31b
Christopher Mühl <christopher@dieschittigs.de>
2018-04-03 17:14:00 +0100

    Bump version to 0.0.6

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     Device: require('./src/Device'),
     Protocol: require('./src/Protocol'),
 
+    Hook: require('./src/Hooks/Hook'),
     Filter: require('./src/Hooks/Filter'),
     Action: require('./src/Hooks/Action'),
     Mutator: require('./src/Hooks/Mutator')
